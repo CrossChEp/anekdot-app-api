8b3c069e9b72a41750402fb750818450fad9146a
Anderzhanov Oleg <anderzanovoleg@gmail.com>
2022-04-11 17:52:02 +0300

    docstrings

--- models/user_methods.py
+++ models/user_methods.py
@@ -6,11 +6,25 @@ from store import User
 
 
 def hash_password(password: str) -> bytes:
+    """hashes password
+
+    :param password: str
+        (user's input password)
+    :return: bytes
+    """
+
     hashed_password = bcrypt.hashpw(password=password.encode(), salt=bcrypt.gensalt())
     return hashed_password
 
 
 def register_user(user_data: UserModel) -> None:
+    """encrypts user's password and adds him to database
+
+    :param user_data: UserModel
+        (user's data)
+    :return: None
+    """
+
     session = next(generate_session())
     user_data.password = hash_password(user_data.password)
     user = User(**user_data.dict())
